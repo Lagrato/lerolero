44fffe2662c25e96cdd450462c98d66a64cc48d7
Thiago Aveiro <thiago.aveiro@usp.br>
2025-03-21 10:30:34 -0300

    digitacao

--- lerolero.py
+++ lerolero.py
@@ -8,7 +8,7 @@ import random
 parte1 = [
 	"O sistema em desenvolvimento",
 	"O novo protocolo de comunicação",
-	"O algoritmo foi otimizado e"
+	"O algoritmo foi otimizado e "
 ]
 parte2 = [
 	"possui excelente desmpenho",
