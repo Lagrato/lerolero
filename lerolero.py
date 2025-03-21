124874b8569f03cee995b74ef0400e0d78fae676
Gabriel Ishicava Caleffi <caleffi131@usp.com>
2025-03-21 10:18:54 -0300

    Insere os trechos das frases

--- lerolero.py
+++ lerolero.py
@@ -5,8 +5,18 @@ Gerar frases de efeito sem significado real."""
 
 import random
 
-parte1 = []
-parte2 = []
-parte3 = []
+parte1 = [
+	"O sistema em desenvolvimento",
+	"O novo protocolo de comunicação",
+	"O algorritmoo otimizado"
+]
+parte2 = [
+	"possui excelente desmpenho",
+	"oferece garantias de precesão acima da média",
+	"preenche uma lacuna significativa"]
+parte3 = [
+	"nas aplicações a que se destina",
+	"em relação às opções disponíveis no mercado"
+	"promovendo ampla vantagem competitiva a seus usuários"]
 
 print (random.choice(parte1), random.choice(parte2), random.choice(parte3))
